0fca7b21b257d8ee3d140467a1e9cd5009147c99
qobuz-dl <113257996+qobuz-dl@users.noreply.github.com>
2022-12-06 20:10:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 LOGGER = logging.getLogger(__name__)
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID", 00000))
-    API_HASH = os.environ.get("API_HASH", "0000")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "00000")
-    OWNER_ID = int(os.environ.get('OWNER_ID', 1257782782)) # give your owner id # if given 0 shell will not works
+    APP_ID = int(os.environ.get("APP_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    OWNER_ID = int(os.environ.get('OWNER_ID', "")) # give your owner id # if given 0 shell will not works
     AUTH_IDS = [int(x) for x in os.environ.get("AUTH_IDS", "0").split()] # if open to everyone give 0
     AUTH_IDS.append(OWNER_ID)
-    QOBUZ_MAIL = os.environ.get("QOBUZ_MAIL", "xxxx")
-    QOBUZ_PASS = os.environ.get("QOBUZ_PASS", "xxxx")
+    QOBUZ_MAIL = os.environ.get("QOBUZ_MAIL", "")
+    QOBUZ_PASS = os.environ.get("QOBUZ_PASS", "")
     QOBUZ_QUAL = int(os.environ.get("QOBUZ_QUAL", 27))
     HEROKU_API_KEY = os.environ.get('HEROKU_API_KEY', None)
     HEROKU_APP_NAME = os.environ.get('HEROKU_APP_NAME', None)
